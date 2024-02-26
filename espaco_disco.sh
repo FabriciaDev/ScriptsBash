2f14216a3d079e212d497f5c2b3b6a011c0c3e23
Fabricia <116111685+FabriciaDev@users.noreply.github.com>
2024-02-26 20:02:15 +0000

    Mon, Feb 26, 2024, 5:02 PM -03:00

--- espaco_disco.sh
+++ espaco_disco.sh
@@ -1,3 +1,10 @@
 # Suponha que você queira verificar o espaço livre em disco usando o comando `df -h`. Você pode
 # incorporar esse comando em um script da seguinte maneira:
-
+```bash
+#!/bin/bash
+espaco_livre=$(df -h)
+echo "Espaço livre em disco:"
+echo "$espaco_livre"
+```
+#Nesse exemplo, o comando `df -h` é executado e seu resultado é armazenado na variável
+#`espaco_livre`. Em seguida, o resultado é exibido usando o comando `echo`.
