100b2346d246416f125f7a631105c05e7c5fc945
Fabricia <116111685+FabriciaDev@users.noreply.github.com>
2024-02-26 19:38:01 +0000

    Mon, Feb 26, 2024, 4:38 PM -03:00

--- log_autenticacao.sh
+++ log_autenticacao.sh
@@ -2,3 +2,6 @@
 # ajudar a detectar possíveis tentativas de invasão. O comando `grep` pode ser usado para
 # procurar padrões de logs.
 
+#!/bin/bash
+echo "Procurando por tentativas de login falhadas nos logs:"
+grep "Failed password" /var/log/auth.log
